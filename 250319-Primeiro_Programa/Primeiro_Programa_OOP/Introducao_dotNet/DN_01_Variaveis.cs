b793fc48be37c8ac1760748acfc6d14d097883f0
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-03-19 17:13:31 -0300

    ✍️ comment

--- 250319-Primeiro_Programa/Primeiro_Programa_OOP/Introducao_dotNet/DN_01_Variaveis.cs
+++ 250319-Primeiro_Programa/Primeiro_Programa_OOP/Introducao_dotNet/DN_01_Variaveis.cs
@@ -6,17 +6,17 @@
         {
             //camelCase
 
-            byte idade = 25;
-            sbyte temperatura = -25;
+            byte idade = 25; // armazena caracteres (8 bits)
+            sbyte temperatura = -25; // números inteiros entre -128 e 127
 
-            short populacao = -1000;
-            ushort identificador = 10;
+            short populacao = -1000; // números inteiros entre -32.768 e 32.767 
+            ushort identificador = 10; // números inteiros até 65.535 (apenas positivos)
 
-            int quantidade = -10;
-            uint quantidade2 = 2;
+            int quantidade = -10; // números inteiros entre -2.147.483.648 e 2.147.483.647 
+            uint quantidade2 = 2; // números inteiros até 4.294.967.295 (apenas positivos)
 
-            long distancia = -10000000000;
-            ulong distancia2 = 10000000000;
+            long distancia = -10000000000; // números inteiros entre -9.223.372.036.854.775.808 e 9.223.372.036.854.775.807 
+            ulong distancia2 = 10000000000; // números inteiros até 18.446.744.073.709.551.615 (apenas positivos)
 
             float altura = 1.71f;
             double peso = 60.5;
@@ -24,7 +24,7 @@
 
             char letra = 'A';
 
-            bool estaChovendo = true; //0 ou 1
+            bool estaChovendo = true; // 0 ou 1
 
             Console.WriteLine(idade);
         }
