13a11c82946b4e21761b305c10e2f148c70eaefb
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-05 14:35:49 -0300

    // create 240425-Checkpoint2

---
+++ 240425-Checkpoint2/CP2.API/Application/Services/FornecedorApplicationService.cs
@@ -0,0 +1,57 @@
+﻿using CP2.API.Application.Interfaces;
+using CP2.API.Application.Dtos;
+using CP2.API.Domain.Entities;
+using CP2.API.Domain.Interfaces;
+
+namespace CP2.API.Application.Services
+{
+    public class FornecedorApplicationService : IFornecedorApplicationService
+    {
+        private readonly IFornecedorRepository _repository;
+
+        public FornecedorApplicationService(IFornecedorRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public FornecedorEntity? ObterFornecedorPorId(int id)
+        {
+            return _repository.ObterPorId(id);
+        }
+
+        public IEnumerable<FornecedorEntity> ObterTodosFornecedores()
+        {
+            return _repository.ObterTodos();
+        }
+
+        public FornecedorEntity SalvarDadosFornecedor(FornecedorDto fornecedorDto)
+        {
+            
+            var fornecedor = new FornecedorEntity
+            {
+                
+            };
+
+            return _repository.Salvar(fornecedor);
+        }
+
+        public FornecedorEntity EditarDadosFornecedor(int id, FornecedorDto fornecedorDto)
+        {
+            var fornecedor = _repository.ObterPorId(id);
+
+            if (fornecedor != null)
+            {
+               
+                return _repository.Editar(fornecedor);
+            }
+
+            return null;
+        }
+
+        public FornecedorEntity DeletarDadosFornecedor(int id)
+        {
+            return _repository.Deletar(id);
+        }
+    }
+
+}
